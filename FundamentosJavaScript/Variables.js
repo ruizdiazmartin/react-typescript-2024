f0c962ce96e030264c0609d525c31ad838270fb3
Martin Ruiz Diaz <martin.ruiz.sp@SofttekProjects.com>
2024-06-24 11:57:27 -0300

    Prueba final git con rama main

--- FundamentosJavaScript/Variables.js
+++ FundamentosJavaScript/Variables.js
@@ -6,4 +6,4 @@ let variable2 = 2; //Inicializacion
 
 const constant1 = 3; // Al ser constante siempre le debo asignar un valor, declaro const cuando es una variable de solo lectura y la puedo cambiar
 
-console.log(variable1, variable2, constant1);
+console.log(variable1, variable2, constant1); ///Prueba final mail

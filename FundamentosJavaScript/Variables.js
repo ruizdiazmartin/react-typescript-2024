7b0eeef238a9183653601cb20cd40d4c66aebe35
Martin Ruiz Diaz <martin.ruiz.sp@SofttekProjects.com>
2024-06-24 12:04:19 -0300

    Ultima prueba de sincronizacion

--- FundamentosJavaScript/Variables.js
+++ FundamentosJavaScript/Variables.js
@@ -6,4 +6,4 @@ let variable2 = 2; //Inicializacion
 
 const constant1 = 3; // Al ser constante siempre le debo asignar un valor, declaro const cuando es una variable de solo lectura y la puedo cambiar
 
-console.log(variable1, variable2, constant1); ///Prueba final mail
+console.log(variable1, variable2, constant1); //Ahora si esta todo sincronizado entre el git y el proyecto.
